d5618f6527e1dd5bce52992b4772329d3296ce41
Noah Cauchi <njcauchi03@gmail.com>
2025-11-15 17:02:28 -0500

    moved chatwindow to seperate component

---
+++ client/src/Components/ChatWindow.tsx
@@ -0,0 +1,62 @@
+import { type Message } from "./Game";
+import { ListGroup, Form, Stack, Button } from "react-bootstrap";
+import { useSocketEmit } from "./SocketContext";
+import { useState, useRef, useEffect } from "react";
+import { usePlayerName } from "./NameContext";
+
+interface Props {
+  messages: Message[];
+  gameName: string;
+  gameOwner: string;
+}
+
+function ChatWindow({ gameName, gameOwner, messages }: Props) {
+  const emit = useSocketEmit();
+  const [message, setMessage] = useState("");
+  const playerName = usePlayerName();
+  const chatEndRef = useRef<HTMLDivElement>(null);
+
+  const handleSendChat = () => {
+    if (message) {
+      emit("CHAT", playerName, message);
+      setMessage("");
+    }
+  };
+
+  useEffect(() => {
+    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages]);
+
+  return (
+    <Stack className="my-auto">
+      <ListGroup className="overflow-y-auto overflow-x-hidden m-4 lobby-chat">
+        {messages.map((message, index) => (
+          <ListGroup.Item key={index}>
+            {message.player == "SERVER123" ? gameName : message.player}
+            {message.player == "SERVER123" && <span style={{ color: "#D2691E" }}>{"(Server)"}</span>}
+            {message.player == gameOwner && <span style={{ color: "blue" }}>{"(Owner)"}</span>}: {message.text}
+          </ListGroup.Item>
+        ))}
+        <div ref={chatEndRef} />
+      </ListGroup>
+      <Stack direction="horizontal" gap={2} className="mx-4">
+        <Form.Control
+          type="text"
+          placeholder="Send Message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") {
+              handleSendChat();
+            }
+          }}
+        />
+        <Button variant="primary" onClick={handleSendChat}>
+          Send
+        </Button>
+      </Stack>
+    </Stack>
+  );
+}
+
+export default ChatWindow;
--- client/src/Components/Lobby.tsx
+++ client/src/Components/Lobby.tsx
@@ -1,26 +1,18 @@
-import { Form, Spinner, Stack, Button, ListGroup } from "react-bootstrap";
+import { Spinner, Stack, Button } from "react-bootstrap";
 import { usePlayerName } from "./NameContext";
 import { useGameState, type Player } from "./Game";
 import { useSocketEmit } from "./SocketContext";
 import { useGameSocket } from "./SocketContext";
-import { useState, useEffect, useRef } from "react";
+import { useState, useEffect } from "react";
 import PlayerBox from "./PlayerBox";
+import ChatWindow from "./ChatWindow";
 
 function Lobby() {
   const [ready, setReady] = useState(false);
   const gameState = useGameState();
-  const [message, setMessage] = useState("");
   const { connected } = useGameSocket();
   const playerName = usePlayerName();
   const emit = useSocketEmit();
-  const chatEndRef = useRef<HTMLDivElement>(null);
-
-  const handleSendChat = () => {
-    if (message) {
-      emit("CHAT", playerName, message);
-      setMessage("");
-    }
-  };
 
   const handleReady = () => {
     const new_ready = !ready;
@@ -50,10 +42,6 @@ function Lobby() {
     );
   };
 
-  useEffect(() => {
-    chatEndRef.current?.scrollIntoView({ behavior: "smooth" });
-  }, [gameState?.messages]);
-
   if (!connected || !gameState) return <Spinner></Spinner>;
 
   return (
@@ -61,7 +49,7 @@ function Lobby() {
       <h1 className="mb-2">{gameState.game_name}</h1>
       <p className="mb-4">Connected: {connected ? "Yes" : "No"}</p>
       <Stack direction="horizontal" gap={4}>
-        <Stack>
+        <Stack className="my-auto">
           {gameState.players.map(createPlayerBox)}
           {[...Array(gameState.max_players - gameState.players.length).keys()].map(createEmptyPlayerBox)}
           <Button variant={ready ? "warning" : "success"} onClick={handleReady} className="mt-2">
@@ -73,35 +61,7 @@ function Lobby() {
             </Button>
           )}
         </Stack>
-        <Stack className="m-auto">
-          <ListGroup className="overflow-y-auto overflow-x-hidden m-4 lobby-chat">
-            {gameState.messages.map((message, index) => (
-              <ListGroup.Item key={index}>
-                {message.player == "SERVER123" ? gameState.game_name : message.player}
-                {message.player == "SERVER123" && <span style={{ color: "#D2691E" }}>{"(Server)"}</span>}
-                {message.player == gameState.game_owner && <span style={{ color: "blue" }}>{"(Owner)"}</span>}:{" "}
-                {message.text}
-              </ListGroup.Item>
-            ))}
-            <div ref={chatEndRef} />
-          </ListGroup>
-          <Stack direction="horizontal" gap={2} className="mx-4">
-            <Form.Control
-              type="text"
-              placeholder="Send Message"
-              value={message}
-              onChange={(e) => setMessage(e.target.value)}
-              onKeyDown={(e) => {
-                if (e.key === "Enter") {
-                  handleSendChat();
-                }
-              }}
-            />
-            <Button variant="primary" onClick={handleSendChat}>
-              Send
-            </Button>
-          </Stack>
-        </Stack>
+        <ChatWindow gameName={gameState.game_name} gameOwner={gameState.game_owner} messages={gameState.messages} />
       </Stack>
     </>
   );
